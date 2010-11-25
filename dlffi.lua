4b7a09886823dfc6d4f34fab738b4c6d2cd49b0f
Ygrex <bi@hl.ru>
2010-11-25 16:07:01 +0300

    allow multi-return functions

--- dlffi.lua
+++ dlffi.lua
@@ -7,11 +7,142 @@ local cast_table = function(func, tbl)
 end;
 -- }}} cast_table()
 
+local Dlffi = {};
+local Dlffi_t = {}; -- types container
+
 -- {{{ load() <-> rawload()
 local rawload = dl.load;
 dl.rawload = rawload;
 dl.load = function (lib, sym, ret, arg, cast)
-	if not cast then cast = cast_table end;
+	-- if a dynamic symbol is loading
+	if type(lib) == "string" then
+		if not cast then cast = cast_table end;
+		if type(ret) == "table" then
+			-- construct multi-return function
+			-- ret may look like
+			--	{ ret = ffi_type_pointer, 2, 3 }
+			--	ret["ret"] is required key
+			-- {{{ substitute given types with pointers
+			local new_arg = {};
+			for i = 1, #arg, 1 do
+				new_arg[i] = arg[i];
+			end;
+			local struct = Dlffi_t:new();
+			if not struct then
+				return nil, "Dlffi_t:new() failed";
+			end;
+			for i = 1, #ret, 1 do
+				local v = ret[i];
+				local t = new_arg[v]
+				if not t then
+					return nil, string.format(
+						"Argument #%d does not exists",
+						i
+					);
+				end;
+				if not struct[t] then
+					struct[t] = { t };
+					if not struct[t] then
+						return nil, string.format(
+							"Structure #%d " ..
+							"construction failed",
+							i
+						);
+					end;
+				end;
+				new_arg[v] = dl.ffi_type_pointer;
+			end;
+			-- }}} substitute given types with pointers
+			-- load symbol with a real prototype
+			local symbol, e =
+				rawload(lib, sym, ret.ret, new_arg, cast);
+			if not symbol then return nil, e end;
+			-- {{{ make a proxy function
+			local f = function(proxy, ...)
+				local arg = proxy.arg;
+				local ret = proxy.ret;
+				local struct = proxy.struct;
+				local symbol = proxy.symbol;
+				local val = {...};
+				local new_val = {};
+				-- {{{ substitute values with pointers
+				for i = 1, #val, 1 do new_val[i] = val[i] end;
+				for cx = 1, #ret, 1 do
+					local i = ret[cx];
+					local cur_v = new_val[i];
+					if not cur_v then
+						new_val[i] = dl.NULL;
+					else -- {{{ wrap value
+					local cur_t = arg[i];
+					-- create buffer
+					local buf, e = dl.dlffi_Pointer(
+						dl.sizeof(cur_t),
+						true
+					);
+					if not buf then return nil, e end;
+					-- initialize buffer
+					e = dl.type_element(
+						buf,
+						struct[cur_t],
+						1,
+						cur_v
+					);
+					if not e then return
+						nil,
+						"type_element() failed"
+					end;
+					-- substitute value
+					new_val[i] = buf;
+					end; -- }}} wrap value
+				end;
+				-- }}} substitute values with pointers
+				-- jump to original symbol
+				local retval, e = symbol(unpack(new_val));
+				if e then return false, e end;
+				-- get returned values
+				local retvals = {};
+				for cx = 1, #ret, 1 do
+					local i = ret[cx];
+					local buf = new_val[i];
+					if buf == dl.NULL then
+						table.insert(ret, dl.NULL);
+					else -- {{{ unwrap value
+					local cur_t = arg[i];
+					e = dl.type_element(
+						buf,
+						struct[cur_t],
+						1
+					);
+					if not e then return nil,
+						"Return value unwrap failed";
+					end;
+					-- push return value
+					table.insert(retvals, e);
+					end; -- }}} unwrap value
+				end;
+				return true, retval, unpack(retvals);
+			end;
+			-- }}} make a proxy function
+			-- {{{ make proxy object
+			local proxy = newproxy(true);
+			if not proxy then
+				return nil, "newproxy() failed";
+			end;
+			local mt = getmetatable(proxy);
+			if not mt then
+				return nil, "newproxy has no metatable";
+			end;
+			mt.__index = mt;
+			mt.__call = f;
+			mt.arg = arg;
+			mt.ret = ret;
+			mt.struct = struct;
+			mt.symbol = symbol;
+			-- }}} make proxy object
+			-- return proxy instead of original one
+			return proxy;
+		end;
+	end;
 	return rawload(lib, sym, ret, arg, cast);
 end;
 -- }}} load() <-> rawload()
@@ -29,8 +160,6 @@ dl.dlffi_Pointer = dlffi_Pointer;
 -- }}} dlffi_Pointer() <-> rawdlffi_Pointer()
 
 -- {{{ Dlffi
-local Dlffi = {};
-
 -- {{{ is_callable(obj) -- if the object can be called
 local function is_callable(obj)
 	if type(obj) == "function" then return true end;
@@ -96,8 +225,6 @@ end;
 -- }}} Dlffi
 
 -- {{{ Dlffi_t
-local Dlffi_t = {}; -- types container
-
 function Dlffi_t:new(k, v)
 	local o = {
 		types = {},
